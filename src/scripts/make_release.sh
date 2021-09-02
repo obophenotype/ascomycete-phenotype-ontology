947c3e64abcfc98a1aa848489df6d79eca810c50
Jim Balhoff <jim@balhoff.org>
2021-09-02 12:21:37 -0400

    Set committer name.

--- src/scripts/make_release.sh
+++ src/scripts/make_release.sh
@@ -8,9 +8,10 @@ RELEASEDATE=`date +%Y-%m-%d`
 BASE="$OBO/$ONT"
 RELEASE_URIBASE="$BASE/releases/$RELEASEDATE/$ONT"
 
+git config --global user.email "${GH_USERNAME}@users.noreply.github.com"
+git config --global user.name "${GH_USERNAME}"
+
 # Assumes being run from root of repository
-ls -la
-git --version
 robot annotate -i src/ontology/$ONT-edit.obo -O $BASE.owl -V $RELEASE_URIBASE.owl --annotation owl:versionInfo $RELEASEDATE convert --check false -f obo -o $ONT.obo convert --check false -f owl -o $ONT.owl
 git add $ONT.owl
 git add $ONT.obo
