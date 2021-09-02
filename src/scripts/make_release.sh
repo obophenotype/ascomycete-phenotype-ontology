5b8fc26ca577f230c69806a9e96312fb81551e40
Jim Balhoff <jim@balhoff.org>
2021-09-02 11:53:23 -0400

    Add debug line.

--- src/scripts/make_release.sh
+++ src/scripts/make_release.sh
@@ -9,7 +9,7 @@ BASE="$OBO/$ONT"
 RELEASE_URIBASE="$BASE/releases/$RELEASEDATE/$ONT"
 
 # Assumes being run from root of repository
-
+ls -la
 robot annotate -i src/ontology/$ONT-edit.obo -O $BASE.owl -V $RELEASE_URIBASE.owl --annotation owl:versionInfo $RELEASEDATE convert --check false -f obo -o $ONT.obo convert --check false -f owl -o $ONT.owl
 git add $ONT.owl
 git add $ONT.obo
